ca3bb9d4c79df6b309f14c630bba80ffb1a923b8
Sharon-B <sharonbagnall@gmail.com>
2021-07-20 10:44:18 +0000

    Customise django admin panel display

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,28 @@ from .models import Product, Category
 
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Tell the admin which fields to display
+    """
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'product_code',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'display_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
