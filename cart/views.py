d20901a5c0a29e1cade0a8a184119b015dcc4e51
Sharon-B <sharonbagnall@gmail.com>
2021-08-01 17:31:34 +0000

    Add error message to remove_from_cart view

--- cart/views.py
+++ cart/views.py
@@ -71,4 +71,5 @@ def remove_from_cart(request, item_id):
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
