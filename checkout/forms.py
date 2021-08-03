2165b674def383409ffcd200e510b6a88b8939dd
Sharon-B <sharonbagnall@gmail.com>
2021-08-03 17:20:26 +0000

    Create OrderForm class in checkout/forms.py

--- checkout/forms.py
+++ checkout/forms.py
@@ -25,7 +25,7 @@ class OrderForm(forms.ModelForm):
             'phone_number': 'Phone Number',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'town_or_city': 'Town or City',
+            'city_town': 'Town or City',
             'county': 'County/State',
             'country': 'Country',
             'postcode': 'Post Code',
