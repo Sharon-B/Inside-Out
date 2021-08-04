6564d9afdb9a9474a753b6f024476232ca8b4bef
Sharon-B <sharonbagnall@gmail.com>
2021-08-04 18:21:16 +0000

    Add stripe variables to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
 
 
 # Create your views here.
 # Checkout view
 def checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "Your cart is empty")
@@ -15,6 +19,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
