004dfa3d3abfd4934b80774c3d787e56c03cd393
Sharon-B <sharonbagnall@gmail.com>
2021-08-07 16:20:31 +0000

    Add payment intent seucceeded/failed methods to StripeWH_Handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 
-class StripeWH_handler:
+class StripeWH_Handler:
     """
     Handle Stripe webhooks
     """
@@ -11,7 +11,23 @@ class StripeWH_handler:
 
     def handle_event(self, event):
         """
-        Handle a generic or unknown event
+        Handle a generic or unknown webhook event
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event[type]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event[type]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
         """
         return HttpResponse(
             content=f'Webhook received: {event[type]}',
