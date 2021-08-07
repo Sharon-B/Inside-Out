dcf9e67b2914ffb432b0bd93a45e0c338a15d2cd
Sharon-B <sharonbagnall@gmail.com>
2021-08-07 18:54:23 +0000

    Create cache_checkout_data view and add its url path

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (render, redirect, reverse,
+                              get_object_or_404, HttpResponse)
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 from .forms import OrderForm
@@ -7,6 +9,7 @@ from products.models import Product
 from cart.contexts import cart_contents
 
 import stripe
+import json
 
 
 # Create your views here.
@@ -109,3 +112,21 @@ def checkout_success(request, order_number):
     }
 
     return render(request, template, context)
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, 'Sorry we cannot process your payment at the moment. \
+            Please try again later.')
+        return HttpResponse(content=e, status=400)
