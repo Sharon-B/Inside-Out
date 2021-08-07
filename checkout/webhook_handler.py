ace4678601830a7250b45f17c7af1ff91dfebf2a
Sharon-B <sharonbagnall@gmail.com>
2021-08-07 19:03:24 +0000

    Test payment intent contains the form data

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -21,6 +21,8 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
+        print(intent)       # Test payment intent contains the form data
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
