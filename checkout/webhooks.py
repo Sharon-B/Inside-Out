ab876706c632bbf73e7903dfd94174dba9187b17
Sharon-B <sharonbagnall@gmail.com>
2021-08-07 17:31:29 +0000

    Add webhook handler to webhook view function

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -34,5 +34,26 @@ def webhook(request):
     except Exception as e:
         return HttpResponse(content=e, status=400)
 
-    print('Success')
-    return HttpResponse(status=200)
+    # print('Success')
+    # return HttpResponse(status=200)
+
+    # Setup webhook handler
+    handler = StripeWH_Handler(request)
+
+    # Map webhook events to their handler functions
+    event_map = {
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed
+    }
+
+    # Get the webhook type from Stripe
+    event_type = event['type']
+
+    # Get the handler from the event map
+    event_handler = event_map.get(event_type, handler.handle_event)
+
+    # Call the event handler with the event
+    response = event_handler(event)
+
+    # return the response to stripe
+    return response
