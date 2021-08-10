f29fc4105dcdf9cee30a8acfabbf4eed170bc13e
Sharon-B <sharonbagnall@gmail.com>
2021-08-10 10:52:37 +0000

    Update order model so order can be uniquely identified, add new fields to admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,12 +12,14 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_cart',
+                       'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name', 'email',
               'phone_number', 'street_address1', 'street_address2',
               'city_town', 'county', 'country', 'postcode',
-              'order_total', 'delivery_cost', 'grand_total',)
+              'order_total', 'delivery_cost', 'grand_total',
+              'original_cart', 'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
