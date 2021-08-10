e1a943d72966a686a4d26fb92a06be2958f148dc
Sharon-B <sharonbagnall@gmail.com>
2021-08-10 21:08:21 +0000

    Add user profile to profile view and render in profile.html

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 
 # Profile view
@@ -6,9 +7,10 @@ def profile(request):
     """
     A view to return the users profile
     """
+    profile = get_object_or_404(UserProfile, user=request.user)
     template = 'user_profiles/profile.html'
     context = {
-
+        'profile': profile,
     }
 
     return render(request, template, context)
