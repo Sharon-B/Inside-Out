cd920b77fa0b0849926108e480bd1f93df67fcd6
Sharon-B <sharonbagnall@gmail.com>
2021-08-11 11:36:27 +0000

    Add user profile form to profile view

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 # Profile view
@@ -8,9 +9,12 @@ def profile(request):
     A view to return the users profile
     """
     profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(instance=profile)
+
     template = 'user_profiles/profile.html'
+
     context = {
-        'profile': profile,
+        'form': form,
     }
 
     return render(request, template, context)
