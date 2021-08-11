708c8295cec8a977053a4ab6f9194819a9a1243c
Sharon-B <sharonbagnall@gmail.com>
2021-08-11 21:25:53 +0000

    Update checkout_success view to save users info, fix typo in checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,9 @@ from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
 from cart.contexts import cart_contents
+from user_profiles.forms import UserProfileForm
+from user_profiles.models import UserProfile
+
 
 import stripe
 import json
@@ -59,7 +62,7 @@ def checkout(request):
                     order.delete()
                     return redirect(reverse('view_cart'))
 
-            request.session['save_info'] = 'save_info' in request.POST
+            request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse(
                             'checkout_success', args=[order.order_number]))
 
@@ -104,6 +107,28 @@ def checkout(request):
 def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Add users profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save users info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_city_town': order.city_town,
+                'default_county': order.county,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f'Your order has been processed successfully! \
         Your order number is {order_number}. A confirmation email \
         will be sent to {order.email}.')
