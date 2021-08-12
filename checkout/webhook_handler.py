9dd4cdaf416dfc086b412fed878057c0d00c2fa8
Sharon-B <sharonbagnall@gmail.com>
2021-08-12 14:21:43 +0000

    Create send_confirmation_email method in webhook_handler.py and implement

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
 from .models import Order, OrderLineItem
 from products.models import Product
 from user_profiles.models import UserProfile
@@ -15,6 +19,28 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """
+        Send a confirmation email to the user
+        """
+        customer_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order}
+        )
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL}
+        )
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [customer_email]
+        )
+
 
     def handle_event(self, event):
         """
@@ -85,6 +111,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | Success: Verified order already exists in database',
                 status=200)
@@ -120,6 +147,7 @@ class StripeWH_Handler:
                     content=f'Webhook received: {event["type"]} | Error: {e}',
                     status=500)
 
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
