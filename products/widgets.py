cf4b0e5f50d336fde9f3e1541347c8d12c76b152
Sharon-B <sharonbagnall@gmail.com>
2021-08-18 19:51:38 +0000

    Update styling of add/edit product and custom widget templates

--- products/widgets.py
+++ products/widgets.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy
 
 # Create custom class for ClearableFileInput widget
 class CustomClearableFileInput(ClearableFileInput):
-    clear_checkbox_label = gettext_lazy('Remove')
+    clear_checkbox_label = gettext_lazy('Remove Image')
     initial_text = gettext_lazy('Current Image')
     input_text = gettext_lazy('')
     template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
