cd1bc358034f63499cbdbc9a8bff90b8079de614
Sharon-B <sharonbagnall@gmail.com>
2021-08-18 11:44:48 +0000

    Update add_product view to redirect to product_detail.html

--- products/views.py
+++ products/views.py
@@ -82,9 +82,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product added successfully!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Please check the form for errors. Product failed to add.')
     else:
