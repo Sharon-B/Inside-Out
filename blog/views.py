fac7280adea3d3a125bff9bf30abaeb3d4f5b5c9
Sharon-B <sharonbagnall@gmail.com>
2021-08-20 09:29:17 +0000

    Create a view to return blog.html and add urlpath

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import BlogPost
 
-# Create your views here.
+
+# All blog posts view
+def all_blog_posts(request):
+    """
+    A view to show the blog page
+    """
+
+    blog_posts = BlogPost.objects.all()
+
+    template = 'blog/blog.html'
+
+    context = {
+        'blog_posts': blog_posts,
+    }
+
+    return render(request, template, context)
