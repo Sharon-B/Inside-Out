115ff7dff142cc52c764d6ef2ed0b0c65102cdbd
Sharon-B <sharonbagnall@gmail.com>
2021-08-20 10:48:16 +0000

    Create BlogComment model in blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,19 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# Blog Comments model
+class BlogComment(models.Model):
+
+    class Meta:
+        ordering = ['-created_on']
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE,
+                                  related_name='comments')
+    comment = models.TextField(max_length=1000, blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return f"Comment on {self.blog_post.title} by {self.user}"
