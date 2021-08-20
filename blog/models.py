c11aef0ce59adf3bdbbe6c5eeff620587195aacc
Sharon-B <sharonbagnall@gmail.com>
2021-08-20 09:23:23 +0000

    Create BlogPost model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Blog post model
+class BlogPost(models.Model):
+
+    class Meta:
+        ordering = ['-created_on']
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='blog_posts')
+    title = models.CharField(max_length=200, unique=True)
+    image = models.ImageField(null=True, blank=True)
+    body_text = models.TextField(default=None, blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.title
