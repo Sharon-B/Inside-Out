448ff03205d4c924a352ba4a1bd216831d29f1ab
Sharon-B <sharonbagnall@gmail.com>
2021-08-22 17:32:27 +0000

    Add new arrivals to home/views.py and index.html template

--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 # Create your views here.
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html')
+    new_arrivals = Product.objects.filter(category__name='new_arrivals')[:4]
+
+    template = 'home/index.html'
+
+    context = {
+        'new_arrivals': new_arrivals,
+    }
+
+    return render(request, template, context)
