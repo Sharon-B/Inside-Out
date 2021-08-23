49ddb26158ef339ec2e4a57b0aa0083d3c03c8bc
Sharon-B <sharonbagnall@gmail.com>
2021-08-23 12:37:36 +0000

    Update debug settings

--- inside_out/settings.py
+++ inside_out/settings.py
@@ -28,7 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['inside-out-sb.herokuapp.com', 'localhost']
 
