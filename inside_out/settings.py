95d8bec2ff3198697d34b31bd734bfde644931c6
Sharon-B <sharonbagnall@gmail.com>
2021-08-24 10:50:49 +0000

    Add cache control to aws settings in settings.py

--- inside_out/settings.py
+++ inside_out/settings.py
@@ -194,6 +194,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # AWS Settings
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000', 
+    }
+
     # S3 Bucket config
     AWS_STORAGE_BUCKET_NAME = 'inside-out-sb'
     AWS_S3_REGION_NAME = 'eu-west-1'
