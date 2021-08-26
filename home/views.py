e07ea8bbeae0cac0acb4b106573bddb594e70ca1
Sharon-B <sharonbagnall@gmail.com>
2021-08-26 13:22:01 +0000

    Add blog_posts to index view

--- home/views.py
+++ home/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from products.models import Product
+from blog.models import BlogPost
 
 
 # Create your views here.
 def index(request):
     """ A view to return the index page """
+
     new_arrivals = Product.objects.filter(category__name='new_arrivals')[:4]
+    blog_posts = BlogPost.objects.all()[:1]
 
     template = 'home/index.html'
 
     context = {
         'new_arrivals': new_arrivals,
+        'blog_posts': blog_posts,
     }
 
     return render(request, template, context)
