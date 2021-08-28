014070cdc15c720656fc2fb33e2d6a603fa97260
Sharon-B <sharonbagnall@gmail.com>
2021-08-28 20:25:54 +0000

    Fix indentation error in for loop in user_profiles/forms.py

--- user_profiles/forms.py
+++ user_profiles/forms.py
@@ -47,6 +47,6 @@ class UserProfileForm(forms.ModelForm):
                     placeholder = f'{placeholders[field]} *'
                 else:
                     placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'profile-form-input'
             self.fields[field].label = labels[field]
