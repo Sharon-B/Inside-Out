79d22caee330804fbe263cf9df370c79ffb49735
Sharon-B <sharonbagnall@gmail.com>
2021-08-30 12:05:02 +0000

    Update formatting urls.py in blog, checkout, user_profiles apps

--- blog/urls.py
+++ blog/urls.py
@@ -6,10 +6,12 @@ urlpatterns = [
     path('<int:blog_post_id>/', views.blog_detail, name='blog_detail'),
     path('add_blog/', views.add_blog_post, name='add_blog'),
     path('edit/<int:blog_post_id>/', views.edit_blog, name='edit_blog'),
-    path('delete/<int:blog_post_id>/',
+    path(
+         'delete/<int:blog_post_id>/',
          views.delete_blog_post,
          name='delete_blog_post'),
-    path('comment/delete/<int:comment_id>/',
+    path(
+         'comment/delete/<int:comment_id>/',
          views.delete_comment,
          name='delete_comment'),
 ]
