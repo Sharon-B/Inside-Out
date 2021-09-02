e4822987b07463d6f474bdd45d6413a5fd550a18
Sharon-B <sharonbagnall@gmail.com>
2021-09-02 18:13:01 +0000

    Set debug = false

--- inside_out/settings.py
+++ inside_out/settings.py
@@ -29,12 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEVELOPMENT' in os.environ
-
-# DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = ['inside-out-sb.herokuapp.com', 'localhost']
-
 
 # Application definition
 
